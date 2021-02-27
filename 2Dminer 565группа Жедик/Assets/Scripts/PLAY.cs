c014e5ea2bdda15bb93feff2f8320e4f1e68c3da
Timofei12 <79372560+Timofei12@users.noreply.github.com>
2021-02-27 19:32:15 +0300

    perviy komit

---
+++ 2Dminer 565группа Жедик/Assets/Scripts/PLAY.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class PLAY : MonoBehaviour
+{
+public void PressedStartLvl( int sceneID )    
+{
+        SceneManager.LoadScene(sceneID);
+    	}
+
+    public void PressedExit()    
+{
+        Application.Quit();
+    	}
+
+}
